7bd9f97d60bd4b488b3219ea68e977c16fb0db9d
Jordan-Templeman <jordant2000@icloud.com>
2026-03-04 19:11:26 -0800

    fix: Align specs and factories with review feedback
    Switch push_event_spec subject from create to build to match the
    convention established by actor_spec and repository_spec. Fix the
    repository factory so full_name and url share a stable owner value
    via a transient attribute, preventing inconsistent Faker evaluations.

--- spec/factories/repositories.rb
+++ spec/factories/repositories.rb
@@ -1,8 +1,12 @@
 FactoryBot.define do
   factory :repository do
+    transient do
+      owner { Faker::Internet.username }
+    end
+
     sequence(:github_id) { |n| 2000 + n }
     name { Faker::App.name.parameterize }
-    full_name { "#{Faker::Internet.username}/#{name}" }
+    full_name { "#{owner}/#{name}" }
     url { "https://api.github.com/repos/#{full_name}" }
   end
 end
--- spec/models/push_event_spec.rb
+++ spec/models/push_event_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe PushEvent, type: :model do
   describe 'validations' do
-    subject { create(:push_event) }
+    subject { build(:push_event) }
 
     it { should validate_presence_of(:github_id) }
     it { should validate_uniqueness_of(:github_id) }
